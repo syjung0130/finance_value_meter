efec7855a47896f0aea256df13d93f1f881c653d
SOONYEONG JUNG <soonyoung87@gmail.com>
2020-08-17 11:11:25 +0900

    재무제표 얻어오는 기능 추가
    재무제표 얻어오는 기능 추가

--- scrap/finance_sheet.py
+++ scrap/finance_sheet.py
@@ -27,21 +27,76 @@ class FinanceSheet():
         print("find_by_corp_name")
         corp_list = self.get_corp_list()
         return corp_list.find_by_corp_name(name, exactly=True)[0]
-    
-    def get_finance_sheet(self, name):
-        print("get_finance_sheet")
-        # 회사 검색
-        corp = self.find_by_corp_name(name)
-        
-        # 2012년부터 연간 연결재무제표 불러오기
-        # TODO: date 표기법 통일 및 전달인자 처리 필요
-        fs = corp.extract_fs(bgn_de='20170101')
+
+    def get_finance_excel(self, name):
+        print("get_finance_excel")
+        self.extract_finance_sheet(name)
         
         # 재무제표 검색 결과를 엑셀파일로 저장 ( 기본저장위치: 실행폴더/fsdata )
-        fs.save()
+        self.fs.save()
+    
+    def update_finance_sheet(self, name):
+        print("update_finance_sheet")
+        self.extract_finance_sheet(name)
+        
+        self.update_balance_sheet()
+        self.update_income_statement()
+        self.update_consolidated_income_statement()
+        self.update_cash_flow()
+    
+    def extract_finance_sheet(self, name):
+        # 회사 검색
+        self.corp = self.find_by_corp_name(name)
+        
+        # 2017년부터 연간 연결재무제표 불러오기
+        # TODO: date 표기법 통일 및 전달인자 처리 필요
+        self.fs = self.corp.extract_fs(bgn_de='20170101')
+        return self.fs
 
+    def update_balance_sheet(self):
+        print("get_balance_sheet")
+        # 연결재무상태표(Balance Sheet)
+        self.df_bs = self.fs['bs'] # 또는 df = fs[0] 또는 df = fs.show('bs')
+        # 연결재무상태표 추출에 사용된 Label 정보
+        self.labels_bs = self.fs.labels['bs']
+        print("===== 연결 재무 상태표 =====")
+        print(type(self.df_bs))
+        print(self.df_bs)
+        print(self.labels_bs)
+
+    def update_income_statement(self):
+        # 연결손익계산서(Incom Statement)
+        self.df_is = self.fs['is'] # 또는 df = fs[1] 또는 df = fs.show('is')
+        # 연결손익계산서 추출에 사용된 Label 정보
+        self.labels_is = self.fs.labels['is']
+        print("===== 연결 손익 계산서 =====")
+        print(type(self.df_is))
+        print(self.df_is)
+        print(self.labels_is)
+    
+    def update_consolidated_income_statement(self):
+        # 연결포괄손익계산서(Consolidated Income Statement)
+        self.df_ci = self.fs['cis'] # 또는 df = fs[2] 또는 df = fs.show('cis')
+        # 연결포괄손익계산서 추출에 사용된 Label 정보
+        self.labels_ci = self.fs.labels['cis']
+        print("===== 연결 포괄 손익 계산서 =====")
+        print(type(self.df_ci))
+        print(self.df_ci)
+        print(self.labels_ci)
+    
+    def update_cash_flow(self):
+        # 현금흐름표(Cash Flow)
+        self.df_cf = self.fs['cf'] # 또는 df = fs[3] 또는 df = fs.show('cf')
+        # 현금흐름표 추출에 사용된 Label 정보
+        self.labels_cf = self.fs.labels['cf']
+        print("===== 현금 흐름표 =====")
+        print(type(self.df_cf))
+        print(self.df_cf)
+        print(self.labels_cf)
+
+    
 
 if __name__ == "__main__":
     # print("check finance data")
     finance_data = FinanceSheet()
-    finance_data.get_finance_sheet('삼성전자')
+    finance_data.update_finance_sheet('삼성전자')
