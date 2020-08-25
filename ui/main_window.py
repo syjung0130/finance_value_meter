049052bbdc8ea33cd2d4732808ffde0ee02ecaaa
SOONYEONG JUNG <soonyoung87@gmail.com>
2020-08-25 20:44:11 +0900

    ui base코드 추가

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,12 +1,12 @@
-import sys
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import qApp, QMainWindow, QAction, QApplication
+from PyQt5.QtWidgets import qApp, QMainWindow, QAction
 from PyQt5.QtGui import QKeySequence
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, widget):
         QMainWindow.__init__(self)
-        self.setWindowTitle("재무제표 분석")
+        self.setWindowTitle("Earthquakes information")
+        self.setCentralWidget(widget)
 
         # Menu
         self.menu = self.menuBar()
@@ -26,9 +26,3 @@ class MainWindow(QMainWindow):
         # Window dimensions
         geometry = qApp.desktop().availableGeometry(self)
         self.setFixedSize(geometry.width() * 0.8, geometry.height() * 0.7)
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    my_window = MainWindow()
-    my_window.show()
-    app.exec_()
