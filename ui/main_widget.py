dfec94272b0e35417dab8efe812a88b9cbe13b4b
SOONYEONG JUNG <soonyoung87@gmail.com>
2020-08-29 16:55:02 +0900

    변수, 함수 표기법 통일

--- ui/main_widget.py
+++ ui/main_widget.py
@@ -19,35 +19,35 @@ class Widget(QWidget):
     def __init__(self):
         QWidget.__init__(self)
 
-        self.grpBox = QGroupBox("재무 정보")
+        self.grp_box = QGroupBox("재무 정보")
         # Create Widgets
-        self.btnStart = QPushButton("시작")
-        self.statusLabel = QLabel("진행 상태:")
-        self.statusLabel.setAlignment(Qt.AlignLeft)
-        self.editCode = QLineEdit("종목 코드 입력")
-        self.btnFinanceSheet = QPushButton("재무 정보 수집")
-        self.btnStart.clicked.connect(self.start_scrap)
-        self.btnFinanceSheet.clicked.connect(self.getCode)
+        self.btn_start = QPushButton("시작")
+        self.status_label = QLabel("진행 상태:")
+        self.status_label.setAlignment(Qt.AlignLeft)
+        self.edit_code = QLineEdit("종목 코드 입력")
+        self.btn_finance_sheet = QPushButton("재무 정보 수집")
+        self.btn_start.clicked.connect(self.start_scrap)
+        self.btn_finance_sheet.clicked.connect(self.get_code)
 
-        # Getting the Model
+        # Getting the Model - PER, PBR, EPS, ROE를 table로
         # self.model = CustomTableModel(data)
         self.model = CustomTableModel()
         # Creating a QTableView
         self.table_view = QTableView()
         self.table_view.setModel(self.model)
 
-        self.leftInnerLayout = QVBoxLayout()
-        self.leftInnerLayout.addWidget(self.btnStart)
-        self.leftInnerLayout.addWidget(self.statusLabel)
-        self.leftInnerLayout.addWidget(self.editCode)
-        self.leftInnerLayout.addWidget(self.btnFinanceSheet)
-        self.leftInnerLayout.setAlignment(Qt.AlignTop)
-        self.grpBox.setLayout(self.leftInnerLayout)
+        self.left_inner_layout = QVBoxLayout()
+        self.left_inner_layout.addWidget(self.btn_start)
+        self.left_inner_layout.addWidget(self.status_label)
+        self.left_inner_layout.addWidget(self.edit_code)
+        self.left_inner_layout.addWidget(self.btn_finance_sheet)
+        self.left_inner_layout.setAlignment(Qt.AlignTop)
+        self.grp_box.setLayout(self.left_inner_layout)
 
-        self.leftLayout = QVBoxLayout()
-        self.leftLayout.addWidget(self.grpBox)
+        self.left_layout = QVBoxLayout()
+        self.left_layout.addWidget(self.grp_box)
 
-        # Right Layout - # Creating QChartView
+        # Right Layout - # Creating QChartView: 매출액, 순이익, 자산 증가 추세
         self.chart = QtChart.QChart()
         self.chart.setAnimationOptions(QtChart.QChart.AllAnimations)
         self.add_series("Magnitude (Column 1)", [0, 1])
@@ -55,23 +55,23 @@ class Widget(QWidget):
         self.chart_view.setRenderHint(QPainter.Antialiasing)
 
         # Right Layout
-        self.rightLayout = QVBoxLayout()
+        self.right_layout = QVBoxLayout()
         # size = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
         # size.setHorizontalStretch(4)
         # self.chart_view.setSizePolicy(size)
-        self.rightLayout.addWidget(self.chart_view)
+        self.right_layout.addWidget(self.chart_view)
 
         # QWidget Layout
         self.main_layout = QHBoxLayout()
-        self.main_layout.addLayout(self.leftLayout)
-        self.main_layout.addLayout(self.rightLayout)
+        self.main_layout.addLayout(self.left_layout)
+        self.main_layout.addLayout(self.right_layout)
 
         # Set the layout to the QWidget
         self.setLayout(self.main_layout)
         
     # Greets the user
-    def getCode(self):
-        print("종목 정보 %s" % self.editCode.text())
+    def get_code(self):
+        print("종목 정보 %s" % self.edit_code.text())
     
     def start_scrap(self):
         print("start scrap")
